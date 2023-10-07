812372de39e9aa56dcacdd18df4197aadfe9bb44
phylix <alirezarastegar117390@gmail.com>
2023-10-07 14:51:31 +0330

    16) Adding province entity attributes and relationships

--- src/main/java/com/alireza/java_code_challenge/entity/Province.java
+++ src/main/java/com/alireza/java_code_challenge/entity/Province.java
@@ -1,4 +1,27 @@
 package com.alireza.java_code_challenge.entity;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
 public class Province {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String name;
+
+    @OneToMany(mappedBy = "province")
+    List<County> countyList;
 }
