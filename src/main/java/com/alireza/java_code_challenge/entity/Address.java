dfc43dbe11f12391542a99a79736646c6095ab27
phylix <alirezarastegar117390@gmail.com>
2023-10-07 14:50:59 +0330

    15) Adding address entity attributes and relationships

--- src/main/java/com/alireza/java_code_challenge/entity/Address.java
+++ src/main/java/com/alireza/java_code_challenge/entity/Address.java
@@ -1,4 +1,25 @@
 package com.alireza.java_code_challenge.entity;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
 public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String description;
+
+    @OneToOne
+    Province province;
 }
