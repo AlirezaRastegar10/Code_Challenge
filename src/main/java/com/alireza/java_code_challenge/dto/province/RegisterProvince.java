b4b8f2ca7a04501bf3947c64c54b5a25665c9418
phylix <alirezarastegar117390@gmail.com>
2023-10-08 14:45:36 +0330

    62) Creating RegisterProvince dto to validation province register information

---
+++ src/main/java/com/alireza/java_code_challenge/dto/province/RegisterProvince.java
@@ -0,0 +1,27 @@
+package com.alireza.java_code_challenge.dto.province;
+
+
+import com.alireza.java_code_challenge.dto.county.RegisterCounty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Builder
+public class RegisterProvince {
+
+    @NotBlank(message = "province name cannot be empty.")
+    @Size(min = 2, message = "province name should have at least 2 characters.")
+    @Pattern(regexp = "(?i)(^[a-z])((?![ .,'-]$)[a-z .,'-])*$", message = "province name can not have number and sign(!,@,#,%,...).")
+    String name;
+
+    @NotNull(message = "the county must not be empty.")
+    RegisterCounty county;
+}
