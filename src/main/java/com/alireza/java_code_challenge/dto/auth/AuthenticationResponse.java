4dd37fb506e3131904f148f459874732c595887e
phylix <alirezarastegar117390@gmail.com>
2023-10-08 15:29:22 +0330

    66) Complete AuthenticationResponse dto implementation

--- src/main/java/com/alireza/java_code_challenge/dto/auth/AuthenticationResponse.java
+++ src/main/java/com/alireza/java_code_challenge/dto/auth/AuthenticationResponse.java
@@ -1,4 +1,16 @@
 package com.alireza.java_code_challenge.dto.auth;
 
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Builder
 public class AuthenticationResponse {
+
+    String token;
 }
