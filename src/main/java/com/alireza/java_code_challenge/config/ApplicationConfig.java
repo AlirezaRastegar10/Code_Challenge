a0f7350768d855c444cc5f89d51e03bf3f6d39c4
phylix <alirezarastegar117390@gmail.com>
2023-10-08 11:11:31 +0330

    40) Adding UserDetailsService bean in this class

--- src/main/java/com/alireza/java_code_challenge/config/ApplicationConfig.java
+++ src/main/java/com/alireza/java_code_challenge/config/ApplicationConfig.java
@@ -1,4 +1,23 @@
 package com.alireza.java_code_challenge.config;
 
+
+import com.alireza.java_code_challenge.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
 public class ApplicationConfig {
+
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findByEmail(username).orElseThrow(
+                () -> new UsernameNotFoundException("no user found with this email: " + username)
+        );
+    }
 }
