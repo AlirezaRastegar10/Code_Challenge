b888db6b5c2458330589e271f56cf85ffd494040
phylix <alirezarastegar117390@gmail.com>
2023-10-08 14:46:45 +0330

    64) Creating RegisterCounty dto to validation county register information

---
+++ src/main/java/com/alireza/java_code_challenge/dto/county/RegisterCounty.java
@@ -0,0 +1,27 @@
+package com.alireza.java_code_challenge.dto.county;
+
+
+import com.alireza.java_code_challenge.dto.city.RegisterCity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Builder
+public class RegisterCounty {
+
+    @NotBlank(message = "county name cannot be empty.")
+    @Size(min = 2, message = "county name should have at least 2 characters.")
+    @Pattern(regexp = "(?i)(^[a-z])((?![ .,'-]$)[a-z .,'-])*$", message = "county name can not have number and sign(!,@,#,%,...).")
+    String name;
+
+    @NotNull(message = "the city must not be empty.")
+    RegisterCity city;
+}
