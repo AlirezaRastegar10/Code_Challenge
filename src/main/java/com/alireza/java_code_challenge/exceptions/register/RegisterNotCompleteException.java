3b253f45bc8bed05d945944aaa4019659cf97048
phylix <alirezarastegar117390@gmail.com>
2023-10-08 17:13:50 +0330

    80) Creating RegisterNotCompleteException class for register errors

---
+++ src/main/java/com/alireza/java_code_challenge/exceptions/register/RegisterNotCompleteException.java
@@ -0,0 +1,8 @@
+package com.alireza.java_code_challenge.exceptions.register;
+
+public class RegisterNotCompleteException extends RuntimeException {
+
+    public RegisterNotCompleteException(String message) {
+        super(message);
+    }
+}
