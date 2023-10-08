3ff801c6fd470094b3c50f4ef2728b8c2ed27f51
phylix <alirezarastegar117390@gmail.com>
2023-10-08 14:43:13 +0330

    60) Adding required imports and properties in this annotation

--- src/main/java/com/alireza/java_code_challenge/utils/annotations/dateofbirth/ValidDateOfBirth.java
+++ src/main/java/com/alireza/java_code_challenge/utils/annotations/dateofbirth/ValidDateOfBirth.java
@@ -1,4 +1,18 @@
 package com.alireza.java_code_challenge.utils.annotations.dateofbirth;
 
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = DateOfBirthValidator.class)
+@Documented
 public @interface ValidDateOfBirth {
+    String message() default "Invalid date of birth";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
 }
