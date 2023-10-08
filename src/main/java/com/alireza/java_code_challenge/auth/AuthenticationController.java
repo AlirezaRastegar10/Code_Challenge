d143356a9e2d7305cecb9cb44897dcf9a997b666
phylix <alirezarastegar117390@gmail.com>
2023-10-08 17:15:08 +0330

    82) Adding register user endpoint to this controller

--- src/main/java/com/alireza/java_code_challenge/auth/AuthenticationController.java
+++ src/main/java/com/alireza/java_code_challenge/auth/AuthenticationController.java
@@ -1,4 +1,25 @@
 package com.alireza.java_code_challenge.auth;
 
+
+import com.alireza.java_code_challenge.dto.auth.AuthenticationResponse;
+import com.alireza.java_code_challenge.dto.auth.RegisterRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
 public class AuthenticationController {
+
+    private final AuthenticationService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@Valid @RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(service.register(request));
+    }
 }
