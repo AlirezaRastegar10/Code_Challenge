d3e56b9398534b24c9ec5752b6e771b99320261c
phylix <alirezarastegar117390@gmail.com>
2023-10-08 14:47:11 +0330

    65) Creating RegisterCity dto to validation city register information

---
+++ src/main/java/com/alireza/java_code_challenge/dto/city/RegisterCity.java
@@ -0,0 +1,22 @@
+package com.alireza.java_code_challenge.dto.city;
+
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Builder
+public class RegisterCity {
+
+    @NotBlank(message = "city name cannot be empty.")
+    @Size(min = 2, message = "city name should have at least 2 characters.")
+    @Pattern(regexp = "(?i)(^[a-z])((?![ .,'-]$)[a-z .,'-])*$", message = "city name can not have number and sign(!,@,#,%,...).")
+    String name;
+}
