131f62015deaaae3297bbb946120294bef5ab8a0
phylix <alirezarastegar117390@gmail.com>
2023-10-08 12:52:43 +0330

    54) Adding required imports and properties to this annotation

--- src/main/java/com/alireza/java_code_challenge/utils/annotations/nationalcode/IranianNationalCode.java
+++ src/main/java/com/alireza/java_code_challenge/utils/annotations/nationalcode/IranianNationalCode.java
@@ -1,4 +1,18 @@
 package com.alireza.java_code_challenge.utils.annotations.nationalcode;
 
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = IranianNationalCodeValidator.class)
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface IranianNationalCode {
+    String message() default "Invalid Iranian National Code";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
 }
