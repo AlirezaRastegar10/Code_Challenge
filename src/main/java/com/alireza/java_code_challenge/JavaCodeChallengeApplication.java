e1e74d8c6a68b4c0a028dc2632ba6c8827902eb4
phylix <alirezarastegar117390@gmail.com>
2023-10-08 17:12:15 +0330

    78) Adding JavaCodeChallengeApplication class as main class for running project

---
+++ src/main/java/com/alireza/java_code_challenge/JavaCodeChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.alireza.java_code_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCodeChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCodeChallengeApplication.class, args);
+    }
+
+}
