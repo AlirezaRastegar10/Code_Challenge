37ec5e8fb93ad0aacbe0b03e583008f796f3e575
phylix <alirezarastegar117390@gmail.com>
2023-10-08 12:51:41 +0330

    53) Complete IranianNationalCodeValidator methods implementation

--- src/main/java/com/alireza/java_code_challenge/utils/annotations/nationalcode/IranianNationalCodeValidator.java
+++ src/main/java/com/alireza/java_code_challenge/utils/annotations/nationalcode/IranianNationalCodeValidator.java
@@ -1,4 +1,31 @@
 package com.alireza.java_code_challenge.utils.annotations.nationalcode;
 
-public class IranianNationalCodeValidator {
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.util.regex.Pattern;
+
+public class IranianNationalCodeValidator implements ConstraintValidator<IranianNationalCode, String> {
+
+    private static final Pattern IRANIAN_NATIONAL_CODE_PATTERN = Pattern.compile("^[0-9]{10}$");
+
+    @Override
+    public void initialize(IranianNationalCode constraintAnnotation) {
+    }
+
+    @Override
+    public boolean isValid(String nationalCode, ConstraintValidatorContext context) {
+        if (nationalCode == null || !IRANIAN_NATIONAL_CODE_PATTERN.matcher(nationalCode).matches()) {
+            return false;
+        }
+
+        // Calculate the checksum of the Iranian national code
+        int[] digits = nationalCode.chars().map(Character::getNumericValue).toArray();
+        int checksum = 0;
+        for (int i = 0; i < 9; i++) {
+            checksum += digits[i] * (10 - i);
+        }
+        checksum %= 11;
+        return checksum < 2 && digits[9] == checksum || checksum >= 2 && digits[9] == 11 - checksum;
+    }
 }
