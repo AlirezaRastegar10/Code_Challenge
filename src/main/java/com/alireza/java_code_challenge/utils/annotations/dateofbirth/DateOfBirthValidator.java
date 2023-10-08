56da37f8dcae3bc642b3e3fc4abcd6b08c7764d4
phylix <alirezarastegar117390@gmail.com>
2023-10-08 14:42:18 +0330

    59) Complete DateOfBirthValidator methods implementation

--- src/main/java/com/alireza/java_code_challenge/utils/annotations/dateofbirth/DateOfBirthValidator.java
+++ src/main/java/com/alireza/java_code_challenge/utils/annotations/dateofbirth/DateOfBirthValidator.java
@@ -1,4 +1,33 @@
 package com.alireza.java_code_challenge.utils.annotations.dateofbirth;
 
-public class DateOfBirthValidator {
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class DateOfBirthValidator implements ConstraintValidator<ValidDateOfBirth, String> {
+
+    @Override
+    public void initialize(ValidDateOfBirth constraintAnnotation) {
+    }
+
+    @Override
+    public boolean isValid(String dateOfBirthStr, ConstraintValidatorContext context) {
+        if (dateOfBirthStr == null || dateOfBirthStr.isEmpty()) {
+            return false;
+        }
+
+        try {
+            LocalDate dateOfBirth = LocalDate.parse(dateOfBirthStr, DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+
+            LocalDate currentDate = LocalDate.now();
+            LocalDate maxBirthDate = currentDate.minusYears(100);
+
+            // Check if the date is not in the future and not more than 100 years old
+            return !dateOfBirth.isAfter(currentDate) && !dateOfBirth.isBefore(maxBirthDate);
+        } catch (Exception e) {
+            return false; // Invalid date format
+        }
+    }
 }
