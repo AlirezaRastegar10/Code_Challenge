e4946411cac754092fc79973c80484a0fa179aa9
phylix <alirezarastegar117390@gmail.com>
2023-10-09 11:37:43 +0330

    95) Adding confirmRegistration end point in this class

--- src/main/java/com/alireza/java_code_challenge/auth/AuthenticationController.java
+++ src/main/java/com/alireza/java_code_challenge/auth/AuthenticationController.java
@@ -3,13 +3,11 @@ package com.alireza.java_code_challenge.auth;
 
 import com.alireza.java_code_challenge.dto.auth.AuthenticationResponse;
 import com.alireza.java_code_challenge.dto.auth.RegisterRequest;
+import com.alireza.java_code_challenge.dto.auth.RegisterResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -19,7 +17,12 @@ public class AuthenticationController {
     private final AuthenticationService service;
 
     @PostMapping("/register")
-    public ResponseEntity<AuthenticationResponse> register(@Valid @RequestBody RegisterRequest request) {
+    public ResponseEntity<RegisterResponse> register(@Valid @RequestBody RegisterRequest request) {
         return ResponseEntity.ok(service.register(request));
+    }
+
+    @PostMapping("/confirm")
+    public ResponseEntity<AuthenticationResponse> confirmRegistration(@RequestParam String email, @RequestParam String confirmationCode) {
+        return ResponseEntity.ok(service.confirmRegistration(email, confirmationCode));
     }
 }
