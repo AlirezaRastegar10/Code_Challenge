79f34eea7aa46d51971e2137b11ecd809118344f
phylix <alirezarastegar117390@gmail.com>
2023-10-09 20:24:27 +0330

    119) Creating UserMapper interface to mapping user dto to user object with mapstruct

---
+++ src/main/java/com/alireza/java_code_challenge/mappers/UserMapper.java
@@ -0,0 +1,19 @@
+package com.alireza.java_code_challenge.mappers;
+
+
+import com.alireza.java_code_challenge.dto.user.AllUserDto;
+import com.alireza.java_code_challenge.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+    @Mapping(source = "address", target = "addressDto")
+    @Mapping(source = "address.province", target = "provinceDto")
+    @Mapping(source = "address.province.countyList", target = "provinceDto.countyList")
+    @Mapping(source = "address.province.countyList.cityList", target = "provinceDto.countyList.cityList")
+    List<AllUserDto> userListToUserDtoList(List<User> userList);
+}
