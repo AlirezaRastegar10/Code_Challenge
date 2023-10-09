0d0e09e80a273ad9c0380230be712f431b4dbcb0
phylix <alirezarastegar117390@gmail.com>
2023-10-09 15:25:44 +0330

    101) Creating AuthenticationRequest dto to validate login user information

---
+++ src/main/java/com/alireza/java_code_challenge/dto/auth/AuthenticationRequest.java
@@ -0,0 +1,27 @@
+package com.alireza.java_code_challenge.dto.auth;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Builder
+public class AuthenticationRequest {
+
+    @NotBlank(message = "email cannot be empty.")
+    @Email(message = "the email pattern is incorrect.")
+    String email;
+
+    @NotBlank(message = "password cannot be empty.")
+    @Size(min = 8, max = 8, message = "password must be 8 characters.")
+    @Pattern(regexp = "^(?=.*?\\d)(?=.*?[a-zA-Z])[a-zA-Z\\d]+$", message = "the password must contain numbers and letters.")
+    String password;
+}
