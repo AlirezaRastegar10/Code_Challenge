0a83169295e5b12467f1c2ad9a95ce6ed5434fa1
phylix <alirezarastegar117390@gmail.com>
2023-10-09 11:25:45 +0330

    86) Change implementation of Province Service

--- src/main/java/com/alireza/java_code_challenge/service/province/ProvinceService.java
+++ src/main/java/com/alireza/java_code_challenge/service/province/ProvinceService.java
@@ -1,10 +1,9 @@
 package com.alireza.java_code_challenge.service.province;
 
 import com.alireza.java_code_challenge.dto.province.RegisterProvince;
-import com.alireza.java_code_challenge.entity.County;
 import com.alireza.java_code_challenge.entity.Province;
 
 public interface ProvinceService {
 
-    Province save(RegisterProvince registerProvince, County county);
+    Province save(RegisterProvince registerProvince);
 }
--- src/main/java/com/alireza/java_code_challenge/service/province/ProvinceServiceImpl.java
+++ src/main/java/com/alireza/java_code_challenge/service/province/ProvinceServiceImpl.java
@@ -2,7 +2,6 @@ package com.alireza.java_code_challenge.service.province;
 
 
 import com.alireza.java_code_challenge.dto.province.RegisterProvince;
-import com.alireza.java_code_challenge.entity.County;
 import com.alireza.java_code_challenge.entity.Province;
 import com.alireza.java_code_challenge.mappers.ProvinceMapperImpl;
 import com.alireza.java_code_challenge.repository.ProvinceRepository;
@@ -17,9 +16,8 @@ public class ProvinceServiceImpl implements ProvinceService {
     private final ProvinceMapperImpl provinceMapper;
 
     @Override
-    public Province save(RegisterProvince registerProvince, County county) {
+    public Province save(RegisterProvince registerProvince) {
         var province = provinceMapper.provinceDtoToProvince(registerProvince);
-        province.getCountyList().add(county);
         return provinceRepository.save(province);
     }
 }
