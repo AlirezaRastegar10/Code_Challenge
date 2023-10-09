174e29747a4209799b3e3df4ee9597b7f8bad633
phylix <alirezarastegar117390@gmail.com>
2023-10-09 11:39:32 +0330

    98) Creating UserAcceptedException class to already user verified

---
+++ src/main/java/com/alireza/java_code_challenge/exceptions/user/UserAcceptedException.java
@@ -0,0 +1,7 @@
+package com.alireza.java_code_challenge.exceptions.user;
+
+public class UserAcceptedException extends RuntimeException {
+    public UserAcceptedException(String message) {
+        super(message);
+    }
+}
