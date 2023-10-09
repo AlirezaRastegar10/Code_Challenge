e87eb4568ccbeef88506bf626735bcb8b50dffa4
phylix <alirezarastegar117390@gmail.com>
2023-10-09 16:44:21 +0330

    107) Creating TokenRepository interface as data access layer to communicate with the token table

---
+++ src/main/java/com/alireza/java_code_challenge/repository/TokenRepository.java
@@ -0,0 +1,19 @@
+package com.alireza.java_code_challenge.repository;
+
+
+import com.alireza.java_code_challenge.entity.Token;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TokenRepository extends JpaRepository<Token, Long> {
+
+    @Query("select t from Token as t where t.user.id=?1 and (t.expired = false or t.revoked = false )")
+    List<Token> findAllValidTokensByUser(Long id);
+
+    Optional<Token> findByToken(String token);
+}
