e3a204cb51850994689145e857b0cb8a7d47e93b
phylix <alirezarastegar117390@gmail.com>
2023-10-10 17:43:42 +0330

    145) Creating PasswordRequest dto for validate user information when change password

---
+++ src/main/java/com/alireza/java_code_challenge/dto/user/PasswordRequest.java
@@ -0,0 +1,34 @@
+package com.alireza.java_code_challenge.dto.user;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Builder
+public class PasswordRequest implements Serializable {
+
+    @NotBlank(message = "email cannot be empty.")
+    @Email(message = "the email pattern is incorrect.")
+    String email;
+
+    @NotBlank(message = "password cannot be empty.")
+    @Size(min = 8, max = 8, message = "password must be 8 characters.")
+    @Pattern(regexp = "^(?=.*?\\d)(?=.*?[a-zA-Z])[a-zA-Z\\d]+$", message = "the password must contain numbers and letters.")
+    String password;
+
+    @NotBlank(message = "repeat password cannot be empty.")
+    @Size(min = 8, max = 8, message = "repeat password must be 8 characters.")
+    @Pattern(regexp = "^(?=.*?\\d)(?=.*?[a-zA-Z])[a-zA-Z\\d]+$", message = "the repeat password must contain numbers and letters.")
+    String repeatPassword;
+}
