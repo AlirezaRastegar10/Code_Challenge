1ea678b5cc353249880813b9eeb7b5bf8dff416c
phylix <alirezarastegar117390@gmail.com>
2023-10-10 09:19:03 +0330

    129) Creating ApiException class to handle unauthorized and forbidden error

---
+++ src/main/java/com/alireza/java_code_challenge/exceptions/api/ApiException.java
@@ -0,0 +1,8 @@
+package com.alireza.java_code_challenge.exceptions.api;
+
+public class ApiException extends RuntimeException {
+
+    public ApiException(String message) {
+        super(message);
+    }
+}
