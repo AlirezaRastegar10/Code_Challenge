fde3df75afdfdc31fbf0a530ea095692d99f5186
phylix <alirezarastegar117390@gmail.com>
2023-10-10 09:40:38 +0330

    131) Adding find by id endpoint to this class

--- src/main/java/com/alireza/java_code_challenge/controller/UserController.java
+++ src/main/java/com/alireza/java_code_challenge/controller/UserController.java
@@ -1,15 +1,13 @@
 package com.alireza.java_code_challenge.controller;
 
 
+import com.alireza.java_code_challenge.dto.user.UserDto;
 import com.alireza.java_code_challenge.entity.enumeration.Role;
 import com.alireza.java_code_challenge.service.user.UserServiceImpl;
 import com.alireza.java_code_challenge.annotations.authorization.HasEndpointAuthorities;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -30,5 +28,11 @@ public class UserController {
             // No pagination requested, fetch all users
             return ResponseEntity.ok(userService.findAll());
         }
+    }
+
+    @GetMapping("/find-by-id/{id}")
+    @HasEndpointAuthorities(authorities = Role.ADMIN)
+    public ResponseEntity<UserDto> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.findById(id));
     }
 }
