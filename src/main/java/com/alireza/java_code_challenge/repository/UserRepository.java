7be84430fbb842efd537909ff04e3669d02e7942
phylix <alirezarastegar117390@gmail.com>
2023-10-10 17:42:06 +0330

    143) Adding find by email and role method in user repository interface

--- src/main/java/com/alireza/java_code_challenge/repository/UserRepository.java
+++ src/main/java/com/alireza/java_code_challenge/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.alireza.java_code_challenge.repository;
 
 import com.alireza.java_code_challenge.entity.User;
+import com.alireza.java_code_challenge.entity.enumeration.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +11,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
+    Optional<User> findByEmailAndRole(String email, Role role);
 }
