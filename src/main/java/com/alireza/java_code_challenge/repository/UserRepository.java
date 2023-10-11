e48d92412ce9985f9ab51636c42411b313c08ee7
phylix <alirezarastegar117390@gmail.com>
2023-10-11 15:50:25 +0330

    171) Adding count user by city query in UserRepository class

--- src/main/java/com/alireza/java_code_challenge/repository/UserRepository.java
+++ src/main/java/com/alireza/java_code_challenge/repository/UserRepository.java
@@ -4,8 +4,10 @@ import com.alireza.java_code_challenge.entity.User;
 import com.alireza.java_code_challenge.entity.enumeration.Role;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -16,4 +18,12 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     @CachePut(value = "cache2", key = "'user_' + #email + #role")
     Optional<User> findByEmailAndRole(String email, Role role);
+
+    @Query("SELECT city.name, COUNT(u) FROM User u " +
+            "JOIN u.address.province.countyList county " +
+            "JOIN county.cityList city " +
+            "WHERE (?1 IS NULL OR city.name = ?1) " +
+            "AND (?2 IS NULL OR u.age >= ?2) " +
+            "GROUP BY city.name")
+    List<Object[]> countUsersByCityAndAge(String cityFilter, Integer minAge);
 }
