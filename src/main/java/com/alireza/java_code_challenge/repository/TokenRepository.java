89e5c4d678b2c491668781493edc93952399ed3e
phylix <alirezarastegar117390@gmail.com>
2023-10-11 14:45:42 +0330

    161) Adding @CachePut annotation for redis caching to token repository methods

--- src/main/java/com/alireza/java_code_challenge/repository/TokenRepository.java
+++ src/main/java/com/alireza/java_code_challenge/repository/TokenRepository.java
@@ -2,6 +2,7 @@ package com.alireza.java_code_challenge.repository;
 
 
 import com.alireza.java_code_challenge.entity.Token;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -12,8 +13,10 @@ import java.util.Optional;
 @Repository
 public interface TokenRepository extends JpaRepository<Token, Long> {
 
+    @CachePut(value = "cache2", key = "'token_user_' + #id")
     @Query("select t from Token as t where t.user.id=?1 and (t.expired = false or t.revoked = false )")
     List<Token> findAllValidTokensByUser(Long id);
 
+    @CachePut(value = "cache2", key = "'token_' + #token")
     Optional<Token> findByToken(String token);
 }
