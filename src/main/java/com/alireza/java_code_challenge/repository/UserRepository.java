fb88fd79496291a658567fd38960a03022ec847e
phylix <alirezarastegar117390@gmail.com>
2023-10-11 14:45:18 +0330

    160) Adding @CachePut annotation for redis caching to user repository methods

--- src/main/java/com/alireza/java_code_challenge/repository/UserRepository.java
+++ src/main/java/com/alireza/java_code_challenge/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.alireza.java_code_challenge.repository;
 
 import com.alireza.java_code_challenge.entity.User;
 import com.alireza.java_code_challenge.entity.enumeration.Role;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,6 +11,9 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    @CachePut(value = "cache2", key = "'user_' + #email")
     Optional<User> findByEmail(String email);
+
+    @CachePut(value = "cache2", key = "'user_' + #email + #role")
     Optional<User> findByEmailAndRole(String email, Role role);
 }
