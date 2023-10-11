36344a51b4bfe21edd64bc3dce77278b3ff265f3
phylix <alirezarastegar117390@gmail.com>
2023-10-11 11:44:14 +0330

    155) Adding findByCountyId method in this repository

--- src/main/java/com/alireza/java_code_challenge/repository/CityRepository.java
+++ src/main/java/com/alireza/java_code_challenge/repository/CityRepository.java
@@ -5,6 +5,10 @@ import com.alireza.java_code_challenge.entity.City;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CityRepository extends JpaRepository<City, Long> {
+
+    Optional<City> findByCountyId(Long countyId);
 }
