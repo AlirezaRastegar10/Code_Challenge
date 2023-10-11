3a0ee7e8a822df9ef6c0df4aa69ba768056a99da
phylix <alirezarastegar117390@gmail.com>
2023-10-11 14:49:46 +0330

    168) Creating ProvinceNotFoundException class for no province found with addressId

---
+++ src/main/java/com/alireza/java_code_challenge/exceptions/province/ProvinceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.alireza.java_code_challenge.exceptions.province;
+
+public class ProvinceNotFoundException extends RuntimeException {
+    public ProvinceNotFoundException(String message) {
+        super(message);
+    }
+}
