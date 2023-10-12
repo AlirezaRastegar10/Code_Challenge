ef1a86899a1b0592d41054450f47fdf04d19da52
phylix <alirezarastegar117390@gmail.com>
2023-10-12 15:28:01 +0330

    191) Creating OpenApiConfig class to config our swagger ui

---
+++ src/main/java/com/alireza/java_code_challenge/config/OpenApiConfig.java
@@ -0,0 +1,44 @@
+package com.alireza.java_code_challenge.config;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Alireza",
+                        email = "alirezarastegar117390@gmail.com"
+                ),
+                description = "OpenApi documentation for User Profile Application",
+                title = "OpenApi specification - Alireza",
+                version = "1.0"
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8080"
+                )
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
