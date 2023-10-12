65d50eea3bace9c03aadbbbb8083e58ca6b89f4b
phylix <alirezarastegar117390@gmail.com>
2023-10-12 15:28:35 +0330

    192) Add swagger urls to permitAll method

--- src/main/java/com/alireza/java_code_challenge/config/SecurityConfiguration.java
+++ src/main/java/com/alireza/java_code_challenge/config/SecurityConfiguration.java
@@ -29,9 +29,20 @@ public class SecurityConfiguration {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth ->
-                        auth.requestMatchers("/api/v1/auth/**").permitAll()
+                        auth.requestMatchers(
+                                        "/api/v1/auth/**",
+                                        "/v2/api-docs",
+                                        "/v3/api-docs",
+                                        "/v3/api-docs/**",
+                                        "/swagger-resources",
+                                        "/swagger-resources/**",
+                                        "/configuration/ui",
+                                        "/configuration/security",
+                                        "/swagger-ui/**",
+                                        "/webjars/**",
+                                        "/swagger-ui.html").permitAll()
                                 .requestMatchers("/admin/**").hasAuthority("ADMIN")
-                        .anyRequest().authenticated())
+                                .anyRequest().authenticated())
                 .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
